205e80346fcd6720fdead87f69a692ff39384d68
Janindumaleesha <janindu.maleesha@app360.lk>
2025-08-26 12:32:13 +0530

    lession two done

--- UnitTestDemo/UnitTestingDemo.Tests/CalculatorServiceTestsUnit.cs
+++ UnitTestDemo/UnitTestingDemo.Tests/CalculatorServiceTestsUnit.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestDemo.Services;
 
 namespace UnitTestingDemo.Tests
 {
     public class CalculatorServiceTestsUnit
     {
         [Fact]
-        public void Should_Be_Always_True()
+        public void ShouldInputTwoIntAndAddThemReturnInt()
         {
-            Assert.True(true);
+            // Given
+            var calService = new CalculatorService();
+
+            int randomIntA = 8;
+            int randomIntB = 7;
+            int expectedValue = 15;
+
+
+            // When
+            var actualValue = calService.AddTwoNumbers(randomIntA, randomIntB);
+
+            // Then
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void ShouldInputTwoDoubleAndAddThemReturnDouble()
+        {
+            // Given
+            var calService = new CalculatorService();
+
+            double randomDoubleA = 8.54;
+            double randomDoubleB = 7.2;
+            double expectedValue = 15.74;
+
+            // When
+            var actualVales = calService.AddTwoDoubles(randomDoubleA, randomDoubleB);
+
+            // Then
+            Assert.Equal(expectedValue, actualVales, 2);
         }
     }
 }
