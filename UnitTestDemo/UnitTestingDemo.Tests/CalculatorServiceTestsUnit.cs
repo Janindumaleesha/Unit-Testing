1ec563cf70f590cd009b6fc10c45a7849b3d2539
Janindumaleesha <janindu.maleesha@app360.lk>
2025-08-26 11:53:56 +0530

    First lession done

---
+++ UnitTestDemo/UnitTestingDemo.Tests/CalculatorServiceTestsUnit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingDemo.Tests
+{
+    public class CalculatorServiceTestsUnit
+    {
+        [Fact]
+        public void Should_Be_Always_True()
+        {
+            Assert.True(true);
+        }
+    }
+}
