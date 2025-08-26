2ea0474421ebbfc209d1f81cffcdf62d52228db9
Janindumaleesha <janindu.maleesha@app360.lk>
2025-08-26 16:41:36 +0530

    Completed lession three.

---
+++ UnitTestDemo/UnitTestingDemo.Tests/StudentServiceTestsUnit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnitTestDemo.Services;
+
+namespace UnitTestingDemo.Tests
+{
+    public class StudentServiceTestsUnit
+    {
+        [Fact]
+        public void ShouldReturnFullNameByGettingFirstNameAndLastName()
+        {
+            // Given
+            var studentService = new StudentService();
+            string randomStringA = "Janindu";
+            string randomStringB = "Maleesha";
+            string expectedValue = "Janindu Maleesha";
+
+            // When
+            string actualValue = studentService.GetFullName(randomStringA, randomStringB);
+
+            // Then
+            //Assert.Equal(expectedValue, actualValue);
+            //Assert.Equal(expectedValue, actualValue, ignoreCase: true);
+            //Assert.Contains(expectedValue, actualValue, StringComparison.CurrentCultureIgnoreCase);
+            Assert.Matches(new Regex("^[A-Za-z]+( [A-Za-z]+)*$"), actualValue);
+        }
+    }
+}
